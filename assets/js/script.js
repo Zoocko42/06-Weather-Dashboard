334a13d17056e77e56e28a0311bdd32e2a031503
Zachariah Schneider <zoocko@gmail.com>
2022-11-06 12:13:36 -0500

    This update adds a geocoding function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 const searchButton = document.getElementById("searchButton");
 const clearButton = document.getElementById("clearSearch");
-const cityButtons = document.getElementById("cityButtons")
+const cityButtons = document.getElementById("cityButtons");
+const searchForm = document.getElementById("searchSection");
 
 const APIkey = "f700a26284d3a4077a271bbd39e39c99"
 
 // This code finds the current date in Unix Timestamp, and then creates an array of both today and the next five days in Unix Timestamps as well.
 var today = moment().format("X");
 const fiveDays = [today, ((parseInt(today)+ 86400).toString()), ((parseInt(today)+ (86400 * 2)).toString()), ((parseInt(today)+ (86400 * 3)).toString()), ((parseInt(today)+ (86400 * 4)).toString()), ((parseInt(today)+ (86400 * 5)).toString())]
-console.log(fiveDays)
+var cityLatLon = []
 
 
 
@@ -20,10 +21,24 @@ if (prevSearchedCities == []) {
     var searchedCities = prevSearchedCities;
 }
 
+// This converts the user input into longitude and latitude.
+function geoCode (city) {
+
+    var geoAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`
+
+    fetch(geoAPI).then((response) => {
+        return response.json()
+     }).then((data) => {
+        cityLatLon.push(data[0].lat)
+        cityLatLon.push(data[0].lon)
+     })
+}
+
 // This function fetches the API data for the requested city.
 function weatherCall (city, date) {
-
-    var openweather = (`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&dt=` + date + `&appid=` + APIkey);
+    geoCode(city)
+    var openweather = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&dt=${date}&appid=${APIkey}`);
+    // var openweather = (`api.openweathermap.org/data/2.5/forecast?lat=${cityLatLon[0]}&lon=${cityLatLon[1]}&appid=${APIkey}`)
 
     fetch(openweather).then((response) => {
         return response.json()
@@ -31,8 +46,6 @@ function weatherCall (city, date) {
         console.log(data)
      })
 }
-
-
 
 // This function first clears the list of previously searched cities, then adds buttons with the cities based on localStorage.
 function searchedCitiesList () {
@@ -50,15 +63,20 @@ searchedCitiesList()
 
 localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 
-// This function reads the selected value input by the user in the search bar, and adds it to the searched cities list in local storage.
+// This function controls what occurs when a user enters a city and 
 function searchCity (event) {
     event.preventDefault();
 
     const inputSearch = document.getElementById("citySearch").value;
+
+    // This section reads the selected value input by the user in the search bar, and adds it to the searched cities list in local storage.
     searchedCities = JSON.parse(localStorage.getItem("searchedCities"))
     searchedCities.push(inputSearch);
     localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
     searchedCitiesList()
+    weatherCall(inputSearch, today)
+    console.log(cityLatLon)
+
 }
 
 // This function clears out the saved searches in local storage.
@@ -71,7 +89,7 @@ function clearSearches (event) {
 }
 
 
-searchButton.addEventListener("click", searchCity)
+searchForm.addEventListener("submit", searchCity)
 clearButton.addEventListener("click", clearSearches)
 
 
