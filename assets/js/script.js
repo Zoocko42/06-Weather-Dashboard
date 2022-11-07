7b9e85da1a3b895f710ecde48e1f23cf6d1315bc
Zachariah Schneider <zoocko@gmail.com>
2022-11-06 21:53:05 -0500

    This update adds the central box for today's forecast and updates the text to reflect the city selected and today's date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,10 @@ const searchForm = document.getElementById("searchSection");
 
 const APIkey = "f700a26284d3a4077a271bbd39e39c99"
 
-// This code finds the current date in Unix Timestamp, and then creates an array of both today and the next five days in Unix Timestamps as well.
-var today = moment().format("X");
-const fiveDays = [today, ((parseInt(today)+ 86400).toString()), ((parseInt(today)+ (86400 * 2)).toString()), ((parseInt(today)+ (86400 * 3)).toString()), ((parseInt(today)+ (86400 * 4)).toString()), ((parseInt(today)+ (86400 * 5)).toString())]
+// This code finds the current date in Unix Timestamp, and then creates an array of both todayUnix and the next five days in Unix Timestamps as well.
+var todayMDY = moment().format("L");
+var todayUnix = moment().format("X");
+const fiveDays = [todayUnix, ((parseInt(todayUnix)+ 86400).toString()), ((parseInt(todayUnix)+ (86400 * 2)).toString()), ((parseInt(todayUnix)+ (86400 * 3)).toString()), ((parseInt(todayUnix)+ (86400 * 4)).toString()), ((parseInt(todayUnix)+ (86400 * 5)).toString())]
 var latLon = []
 
 // This section checks local storage to see if cities have been searched by the user. If they have, the cities
@@ -69,12 +70,16 @@ function searchCity (event) {
 
     const inputSearch = document.getElementById("citySearch").value;
 
+    var cityAndDay = document.getElementById("cityAndDay")
+
+    cityAndDay.innerHTML = `${inputSearch} (${todayMDY})`
+
     // This section reads the selected value input by the user in the search bar, and adds it to the searched cities list in local storage.
     searchedCities = JSON.parse(localStorage.getItem("searchedCities"))
     searchedCities.push(inputSearch);
     localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
     searchedCitiesList()
-    weatherCall(inputSearch, today)
+    weatherCall(inputSearch, todayUnix)
 }
 
 // This function clears out the saved searches in local storage.
